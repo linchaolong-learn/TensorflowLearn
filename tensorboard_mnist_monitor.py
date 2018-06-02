1e13ae35350f733623e8d86966a93bf73658bf75
linchaolong <linchaolong@healthmall.cn>
2018-06-02 18:09:28 +0800

    data visualisation using tensorboard

--- tensorboard_mnist_monitor.py
+++ tensorboard_mnist_monitor.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
-import mnist_inference
-import os
-from tensorflow.contrib.tensorboard.plugins import projector
 from tensorflow.examples.tutorials.mnist import input_data
+
+import mnist_inference
 
 BATCH_SIZE = 100
 LEARNING_RATE_BASE = 0.8
@@ -11,18 +10,12 @@ REGULARIZATION_RATE = 0.0001
 TRAINING_STEPS = 10000
 MOVING_AVERAGE_DECAY = 0.99
 
-LOG_DIR = 'log'
-SPRITE_FILE = 'mnist_sprite.jpg'
-META_FIEL = "mnist_meta.tsv"
-TENSOR_NAME = "FINAL_LOGITS"
-
 
 def train(mnist):
     #  输入数据的命名空间。
     with tf.name_scope('input'):
         x = tf.placeholder(tf.float32, [None, mnist_inference.INPUT_NODE], name='x-input')
         y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
-
     regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
     y = mnist_inference.inference(x, regularizer)
     global_step = tf.Variable(0, trainable=False)
@@ -51,51 +44,33 @@ def train(mnist):
         with tf.control_dependencies([train_step, variables_averages_op]):
             train_op = tf.no_op(name='train')
 
+    writer = tf.summary.FileWriter("log", tf.get_default_graph())
+
     # 训练模型。
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
-            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
 
             if i % 1000 == 0:
-                print("After %d training step(s), loss on training batch is %g." % (i, loss_value))
+                # 配置运行时需要记录的信息。
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                # 运行时记录运行信息的proto。
+                run_metadata = tf.RunMetadata()
+                _, loss_value, step = sess.run(
+                    [train_op, loss, global_step], feed_dict={x: xs, y_: ys},
+                    options=run_options, run_metadata=run_metadata)
+                writer.add_run_metadata(run_metadata=run_metadata, tag=("step%d" % i), global_step=i)
+                print("After %d training step(s), loss on training batch is %g." % (step, loss_value))
+            else:
+                _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
 
-        final_result = sess.run(y, feed_dict={x: mnist.test.images})
-
-    return final_result
-
-
-def visualisation(final_result):
-    y = tf.Variable(final_result, name=TENSOR_NAME)
-    summary_writer = tf.summary.FileWriter(os.path.join(LOG_DIR, "modified_mnist_train.log"), tf.get_default_graph())
-
-    config = projector.ProjectorConfig()
-    embedding = config.embeddings.add()
-    embedding.tensor_name = y.name
-
-    # Specify where you find the metadata
-    # embedding.metadata_path = META_FIEL
-
-    # Specify where you find the sprite (we will create this later)
-    # embedding.sprite.image_path = SPRITE_FILE
-    # embedding.sprite.single_image_dim.extend([28, 28])
-
-    # Say that you want to visualise the embeddings
-    projector.visualize_embeddings(summary_writer, config)
-
-    sess = tf.InteractiveSession()
-    sess.run(tf.global_variables_initializer())
-    saver = tf.train.Saver()
-    saver.save(sess, os.path.join(LOG_DIR, "modified_mnist_train"), TRAINING_STEPS)
-
-    summary_writer.close()
+    writer.close()
 
 
 def main(argv=None):
     mnist = input_data.read_data_sets("mnist_data", one_hot=True)
-    final_result = train(mnist)
-    visualisation(final_result)
+    train(mnist)
 
 
 if __name__ == '__main__':
